90bb70754bdf724ae128bbee6e18467907e1c168
NetoBasso <baxnetodev@gmail.com>
2023-03-13 10:08:11 -0300

    Fix Experiencies page

--- src/pages/experience/experience.jsx
+++ src/pages/experience/experience.jsx
@@ -12,73 +12,65 @@ const Experience = () => {
       <VerticalTimeline lineColor="#3e497a">
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2015-2016"
-          iconStyle={{ background: "#3e497a", color: '#fff' }}
+          date="2015-2018"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<WorkIcon />}
         >
-           <h3 className="vertical-timeline-element-title">
-            Chef/Manager -  Pizza Express
+          <h3 className="vertical-timeline-element-title">
+            Chef/Manager - Pizza Express
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
             Londres, England
           </h4>
-          <p>Daily Operation supervision, including menu planning, food preparation and kitchen management staff.</p>
+          <p>Supervisão de operações diárias e gerenciamento do staff.</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
           date="2016-2018"
-          iconStyle={{ background: "#3e497a", color: '#fff' }}
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<WorkIcon />}
         >
-           <h3 className="vertical-timeline-element-title">
-            Team Manager -  Noonan Cleaning Limited
+          <h3 className="vertical-timeline-element-title">
+            Software Tester - Medtronic, Medical Technology, Services and
+            Solutions Global
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
             Galway, Ireland
           </h4>
-          <p>Supervise team of cleaners and ensure that they are performing their duties effectively and efficiently. My responsibilities include managing work schedules, assigning tasks, training new team members, and monitoring the quality of work..</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2018-2021"
-          iconStyle={{ background: "#3e497a", color: '#fff' }}
-          icon={<WorkIcon />}
-        >
-           <h3 className="vertical-timeline-element-title">
-            Software Tester - Medtronic, Medical Technology, Services and Solutions Global
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Galway, Ireland
-          </h4>
-          <p>Install and test the E360 and Puritan Bennett 980 ventilator models to ensure they meet the required specifications and performance standards.</p>
+          <p>
+            Instalação e testes de softwares de respiradores modelo E360 e
+            Puritan Bennet.
+          </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
           date="2021-2022"
-          iconStyle={{ background: "#3e497a", color: '#fff' }}
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<WorkIcon />}
         >
-           <h3 className="vertical-timeline-element-title">
-            Legal intern - Baciclides Basso Junior Individual Law Firm
+          <h3 className="vertical-timeline-element-title">
+            Estagiário Jurídico - Baciclides Basso Junior Individual Law Firm
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
             Americana, Brazil
           </h4>
-          <p>Assist lawyers and other legal professionals with a variety of tasks, including conducting research, drafting legal documents, and attending client meetings and court proceedings.</p>
+          <p>laboração de petições, pesquisas, atendimento a clientes.</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
           date="2022-2023"
-          iconStyle={{ background: "#3e497a", color: '#fff' }}
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<WorkIcon />}
         >
-           <h3 className="vertical-timeline-element-title">
-            Front End Developr - Freelancer
+          <h3 className="vertical-timeline-element-title">
+            Desenvolvedor Frontend - Freelancer
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
             São José do Rio Preto, Brazil
           </h4>
-          <p>Freelance web developer, specialize in building projects using React, JavaScript, and TypeScript. I work independently to develop and deliver high-quality web applications,</p>
+          <p>
+            Desenvolvimento de aplicações em JavaScrit Vanila e ReactJS. Criação e implementação de layouts usando Figma, HTML, CSS, Syled-components e Tailwind
+          </p>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
