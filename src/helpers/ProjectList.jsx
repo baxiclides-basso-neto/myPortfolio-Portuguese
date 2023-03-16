b60c8a2c7cb826fe1f51a9f0fc88e8409f27119b
NetoBasso <baxnetodev@gmail.com>
2023-03-16 15:38:34 -0300

    Add new project and fix Display Component

--- src/helpers/ProjectList.jsx
+++ src/helpers/ProjectList.jsx
@@ -3,49 +3,63 @@ import Project2 from "../assets/Project2.png";
 import Project3 from "../assets/Project3.png";
 import Project4 from "../assets/Project4.png";
 import Project5 from "../assets/Project5.png";
+import Project6 from "../assets/Project6.png";
 
 export const ProjectsList = [
   {
     name: "Weather Radar",
     image: Project1,
     skill: "JavaScript, HTML, CSS",
-    githubUrl: 'https://github.com/baxiclides-basso-neto/weatherapplication',
-    vercelUrl: 'https://baxiclides-basso-neto.github.io/weatherapplication/',
-    id: '0'
-
+    githubUrl: "https://github.com/baxiclides-basso-neto/weatherapplication",
+    vercelUrl: "https://baxiclides-basso-neto.github.io/weatherapplication/",
+    id: "0",
   },
   {
     name: "English Dictionary",
     image: Project2,
     skill: "JavaScript, HTML, CSS",
-    githubUrl: 'https://github.com/baxiclides-basso-neto/dictonaryApp',
-    vercelUrl: 'https://baxiclides-basso-neto.github.io/dictonaryApp/',
-    id: '1',
+    githubUrl: "https://github.com/baxiclides-basso-neto/dictonaryApp",
+    vercelUrl: "https://baxiclides-basso-neto.github.io/dictonaryApp/",
+    id: "1",
   },
   {
     name: "Kenzie Hub",
     image: Project3,
     skill: "ReactJS, JavaScript, CSS",
-    githubUrl: 'https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-kenzie-hub-baxiclides-basso-neto',
-    vercelUrl: 'https://react-entrega-kenzie-hub-baxiclides-basso-neto.vercel.app/',
-    id: '3'
+    githubUrl:
+      "https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-kenzie-hub-baxiclides-basso-neto",
+    vercelUrl:
+      "https://react-entrega-kenzie-hub-baxiclides-basso-neto.vercel.app/",
+    id: "3",
   },
   {
     name: "Burger App JS",
     image: Project4,
     skill: "ReactJS, JavaScript, Styled-components",
-    githubUrl: 'https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-hamburgueria-da-kenzie-baxiclides-basso-neto',
-    vercelUrl: 'https://react-entrega-hamburgueria-da-kenzie-baxiclides-basso-neto.vercel.app/',
-    id: '4'
+    githubUrl:
+      "https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-hamburgueria-da-kenzie-baxiclides-basso-neto",
+    vercelUrl:
+      "https://react-entrega-hamburgueria-da-kenzie-baxiclides-basso-neto.vercel.app/",
+    id: "4",
   },
   {
     name: "Burger App TS",
     image: Project5,
-    skill: "ReactJS, TypeScript, HTML, Styled-components",
-    githubUrl: 'https://github.com/Kenzie-Academy-Brasil-Developers/kenzie-burguer-v2-typescript-baxiclides-basso-neto',
-    vercelUrl: 'https://kenzie-burguer-v2-typescript-baxiclides-basso-neto.vercel.app/',
-    id: '5'
-  
+    skill: "ReactJS, TypeScript, HTML, Styled-components, React-Hook-Form, React-Router-Dom ",
+    githubUrl:
+      "https://github.com/Kenzie-Academy-Brasil-Developers/kenzie-burguer-v2-typescript-baxiclides-basso-neto",
+    vercelUrl:
+      "https://kenzie-burguer-v2-typescript-baxiclides-basso-neto.vercel.app/",
+    id: "5",
+  },
+  {
+    name: "Advocacia - Baciclides",
+    image: Project6,
+    skill: "ReactJS, JavaScript, HTML, Styled-components, React-Hook-Form, React-Router-Dom",
+    githubUrl:
+      "https://github.com/baxiclides-basso-neto/baciclides-advocacia",
+    vercelUrl:
+      "https://baciclides-advocacia-baxiclides-basso-neto.vercel.app/",
+    id: "6",
   },
 ];
-
