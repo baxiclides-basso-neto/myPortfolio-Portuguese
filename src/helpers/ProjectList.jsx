b5a93b5ec181d4e8b3457716bdc2285701f335fe
NetoBasso <baxnetodev@gmail.com>
2023-03-24 18:10:20 -0300

    Delete project

--- src/helpers/ProjectList.jsx
+++ src/helpers/ProjectList.jsx
@@ -1,7 +1,6 @@
 import Project1 from "../assets/Project1.png";
 import Project2 from "../assets/Project2.png";
 import Project3 from "../assets/Project3.png";
-import Project4 from "../assets/Project4.png";
 import Project5 from "../assets/Project5.png";
 import Project6 from "../assets/Project6.png";
 import Project7 from "../assets/Project7.png";
@@ -10,20 +9,12 @@ import Project9 from "../assets/Project9.png";
 
 export const ProjectsList = [
   {
-    name: "AbraãoAdvocacia - App",
-    image: Project7,
-    skill: "NextJS, JavaScript, Styled-Components",
-    githubUrl: "https://github.com/baxiclides-basso-neto/abraaoadvogados-app",
-    vercelUrl: "https://abraaoadvogados-app.vercel.app/",
-    id: "0",
-  },
-  {
-    name: "TucsonAdvocacia - App",
-    image: Project8,
-    skill: "NextJS, JavaScript, Styled-Components",
-    githubUrl: "https://github.com/baxiclides-basso-neto/tucsonadvocacia-app",
-    vercelUrl: "https://tucsonadvocacia-app.vercel.app/",
-    id: "0",
+    name: "Weather Radar",
+    image: Project1,
+    skill: "JavaScript, HTML, CSS",
+    githubUrl: "https://github.com/baxiclides-basso-neto/weatherapplication",
+    vercelUrl: "https://baxiclides-basso-neto.github.io/weatherapplication/",
+    id: "1",
   },
   {
     name: "English Dictionary",
@@ -31,7 +22,7 @@ export const ProjectsList = [
     skill: "JavaScript, HTML, CSS",
     githubUrl: "https://github.com/baxiclides-basso-neto/dictonaryApp",
     vercelUrl: "https://baxiclides-basso-neto.github.io/dictonaryApp/",
-    id: "1",
+    id: "2",
   },
   {
     name: "Kenzie Hub",
@@ -43,20 +34,12 @@ export const ProjectsList = [
       "https://react-entrega-kenzie-hub-baxiclides-basso-neto.vercel.app/",
     id: "3",
   },
-  {
-    name: "Burger App JS",
-    image: Project4,
-    skill: "ReactJS, JavaScript, Styled-components",
-    githubUrl:
-      "https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-hamburgueria-da-kenzie-baxiclides-basso-neto",
-    vercelUrl:
-      "https://react-entrega-hamburgueria-da-kenzie-baxiclides-basso-neto.vercel.app/",
-    id: "4",
-  },
+
   {
     name: "Burger App TS",
     image: Project5,
-    skill: "ReactJS, TypeScript, HTML, Styled-components, React-Hook-Form, React-Router-Dom ",
+    skill:
+      "ReactJS, TypeScript, HTML, Styled-components, React-Hook-Form, React-Router-Dom ",
     githubUrl:
       "https://github.com/Kenzie-Academy-Brasil-Developers/kenzie-burguer-v2-typescript-baxiclides-basso-neto",
     vercelUrl:
@@ -66,20 +49,27 @@ export const ProjectsList = [
   {
     name: "Advocacia - Baciclides",
     image: Project6,
-    skill: "ReactJS, JavaScript, HTML, Styled-components, React-Hook-Form, React-Router-Dom",
-    githubUrl:
-      "https://github.com/baxiclides-basso-neto/baciclides-advocacia",
-    vercelUrl:
-      "https://baciclides-advocacia-baxiclides-basso-neto.vercel.app/",
+    skill:
+      "ReactJS, JavaScript, HTML, Styled-components, React-Hook-Form, React-Router-Dom",
+    githubUrl: "https://github.com/baxiclides-basso-neto/baciclides-advocacia",
+    vercelUrl: "https://baciclides-advocacia-baxiclides-basso-neto.vercel.app/",
     id: "6",
   },
   {
-    name: "Weather Radar",
-    image: Project1,
-    skill: "JavaScript, HTML, CSS",
-    githubUrl: "https://github.com/baxiclides-basso-neto/weatherapplication",
-    vercelUrl: "https://baxiclides-basso-neto.github.io/weatherapplication/",
-    id: "0",
+    name: "AbraãoAdvocacia - App",
+    image: Project7,
+    skill: "NextJS, JavaScript, Styled-Components",
+    githubUrl: "https://github.com/baxiclides-basso-neto/abraaoadvogados-app",
+    vercelUrl: "https://abraaoadvogados-app.vercel.app/",
+    id: "7",
+  },
+  {
+    name: "TucsonAdvocacia - App",
+    image: Project8,
+    skill: "NextJS, JavaScript, Styled-Components",
+    githubUrl: "https://github.com/baxiclides-basso-neto/tucsonadvocacia-app",
+    vercelUrl: "https://tucsonadvocacia-app.vercel.app/",
+    id: "8",
   },
   {
     name: "OliveiraAdvocacia - App",
@@ -87,6 +77,6 @@ export const ProjectsList = [
     skill: "JavaScript, HTML, CSS",
     githubUrl: "https://github.com/baxiclides-basso-neto/oliveiraadvogados-app",
     vercelUrl: "https://oliveiraadvogados-app.vercel.app/",
-    id: "0",
-  }
+    id: "9",
+  },
 ];
